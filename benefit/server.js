eff59152de648ca98e1e8716fd9b1dc3ec00f890
Skopi Irwin <skopiirwin@gmail.com>
2024-09-23 21:15:17 +0200

    Add start script and server file for Heroku deployment

--- benefit/server.js
+++ benefit/server.js
@@ -3,14 +3,14 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the Vue app build directory
-app.use(express.static(path.join(__dirname, '/dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 // Handles any requests that don't match the ones above
-app.get('*', (req, res) =>{
-    res.sendFile(path.join(__dirname, '/dist/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
